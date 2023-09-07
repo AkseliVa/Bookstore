7ded2ef6b71912039cef6440e33a732ec2e885bc
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-09-07 12:19:47 +0300

    ekat projektilisäykset

---
+++ src/main/java/hh/sof03/Bookstore/domain/Book.java
@@ -0,0 +1,10 @@
+package hh.sof03.Bookstore.domain;
+
+public class Book {
+
+	private String title;
+	private String author;
+	private Integer year;
+	private String isbn;
+	private Integer price;
+}
---
+++ src/main/java/hh/sof03/Bookstore/webController/BookController.java
@@ -0,0 +1,13 @@
+package hh.sof03.Bookstore.webController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+	@GetMapping("/index")
+	public String Book() {
+		return "";
+	}
+}
