5ac204ef26a84f9422d601ceaab787e30ce26480
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-09-16 14:01:25 +0300

    added delete and add functionality

--- src/main/java/hh/sof03/Bookstore/webController/BookController.java
+++ src/main/java/hh/sof03/Bookstore/webController/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import hh.sof03.Bookstore.domain.Book;
 import hh.sof03.Bookstore.domain.BookRepository;
@@ -22,5 +25,23 @@ public class BookController {
 		model.addAttribute("books", books);
 		
 		return "booklist";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		bookRepository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	
+	@RequestMapping("/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@PostMapping("/save")
+	public String save(Book book) {
+		bookRepository.save(book);
+		return "redirect:booklist";
 	}
 }
